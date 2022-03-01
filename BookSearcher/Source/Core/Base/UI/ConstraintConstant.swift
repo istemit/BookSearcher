b9d99fa3cd21b06892cd4050cff8e15a2dc8fe51
Arman Turalin <tursat1@mail.ru>
2022-03-01 23:51:33 +0300

    Add basic constraint constants

---
+++ BookSearcher/Source/Core/Base/UI/ConstraintConstant.swift
@@ -0,0 +1,20 @@
+//
+//  ConstraintConstant.swift
+//  BookSearcher
+//
+//  Created by Arman Turalin on 01.03.2022.
+//
+
+import UIKit
+
+enum ConstraintConstant {
+    /// 4.0
+    static let small: CGFloat = 4.0
+    
+    /// 8.0
+    static let medium: CGFloat = 8.0
+    
+    /// 16.0
+    static let large: CGFloat = 16.0
+}
+
